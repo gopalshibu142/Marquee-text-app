c600e80240a1e7cfba7951d2286afa1272c5c76a
Gopal S <78960894+MaDGaMeR142@users.noreply.github.com>
2022-12-14 22:24:44 +0530

    v0.1.2

--- lib/main.dart
+++ lib/main.dart
@@ -276,3 +276,194 @@ class _MyAppState extends State<MyApp> {
                   ],
                 ),
               ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("Gradient  :"),
+                  Switch(
+                      activeColor: ui.white,
+                      value: grad,
+                      onChanged: (b) {
+                        setState(() {
+                          grad = b;
+                        });
+                      })
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: 300,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Font Size   : "),
+                    Slider(
+                        value: val,
+                        activeColor: ui.white,
+                        inactiveColor: ui.mid,
+                        onChanged: (v) {
+                          setState(() {
+                            val = v;
+                            fontsize = 100 + v * 200;
+                          });
+                        }),
+                    Text('${fontsize.round()}')
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: 300,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("speed   : "),
+                    Slider(
+                        activeColor: ui.white,
+                        inactiveColor: ui.mid,
+                        value: speed,
+                        onChanged: (v) {
+                          setState(() {
+                            speed = v;
+                          });
+                        }),
+                    Text('${speed.toStringAsPrecision(1)}')
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              OpenContainer(
+                closedBuilder: (context, _) => GestureDetector(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 2, color: ui.white),
+                        color: ui.mid,
+                        borderRadius: BorderRadius.circular(10)),
+                    width: 100,
+                    height: 40,
+                    alignment: Alignment.center,
+                    child: Text("Submit"),
+                  ),
+                ),
+                openBuilder: (context, _) => maarquee(),
+                closedColor: Colors.transparent,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.rotate_90_degrees_ccw, color: ui.white),
+                  Text(" Tilt your phone to the left after submitting"),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container maarquee() {
+    var txt;
+    print(control.text.length);
+    control.text.isEmpty
+        ? txt = "404 No Message found"
+        : txt = " " * 20 + control.text;
+    return Container(
+      decoration:
+          BoxDecoration(color: ui.bg, gradient: grad ? ui.getGrad() : null),
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Center(
+          child: Marquee(
+            velocity: 100 + speed * 400,
+            blankSpace: 300,
+            style: TextStyle(fontSize: fontsize, color: ui.font),
+            textDirection: TextDirection.ltr,
+            text: txt,
+          ),
+        ),
+      ),
+    );
+  }
+
+  showAppDialog(BuildContext context, chk) {
+    print("Showing app dialog");
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Color(0xff111111),
+            scrollable: true,
+
+            content: Container(
+              color: Colors.black,
+              height: 358,
+              //width: 300,
+              child: ColorPicker(
+                  labelTypes: [],
+                  colorPickerWidth: 248,
+                  pickerColor: chk == 1
+                      ? ui.font
+                      : chk == 0
+                          ? ui.bg
+                          : chk == 2
+                              ? ui.grad1
+                              : ui.grad2,
+                  onColorChanged: (color) {
+                    setState(() {
+                      if (chk == 1)
+                        ui.font = color;
+                      else if (chk == 0)
+                        ui.bg = color;
+                      else if (chk == 2)
+                        ui.grad1 = color;
+                      else if (chk == 3) ui.grad2 = color;
+                    });
+                  }),
+            ),
+            //icon: const Icon(Icons.delete),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("OK"),
+              ),
+            ],
+          );
+        });
+  }
+
+  void _initSpeech() async {
+    _speechEnabled = await stt.initialize();
+    setState(() {});
+  }
+
+  void _startListening() async {
+    await stt.listen(onResult: _onSpeechResult);
+  }
+
+  void _stopListening() async {
+    await stt.stop();
+    setState(() {});
+  }
+
+  void _onSpeechResult(var result) {
+    setState(() {
+      rec = false;
+      control.text = result.recognizedWords;
+    });
+  }
+}
